bedf9c3f430bdafd765724dcb7e71d58a0538bcf
Hugrún Hannesdóttir <hugrun17@ru.is>
2018-05-03 14:09:29 +0000

    Project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookCave.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
