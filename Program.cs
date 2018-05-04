56dbe3cf5d7d6dc0d35cbe34ff23887f1614fa25
nannalop <natalia17@ru.is>
2018-05-04 16:10:48 +0000

    database fixes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BookCave.Data;
+using BookCave.Data.EntityModels;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,12 +16,29 @@ namespace BookCave
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            SeedData();
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        public static void SeedData()
+        {
+            var db = new DataContext();
+            if (!db.Authors.Any())
+            {
+                var initialAuthors = new List<Author>()
+                {
+                    new Author { Name = "JK Rowling", Rating = 5 },
+                    new Author { Name = "JR Tolkien", Rating = 5 }
+                };
+                db.AddRange(initialAuthors);
+                db.SaveChanges();
+            }
+            
+        }
     }
 }
--- Repositories/AuthorRepo.cs
+++ Repositories/AuthorRepo.cs
@@ -1,16 +1,25 @@
 using System.Collections.Generic;
+using BookCave.Data;
 using BookCave.Models.ViewModels;
+using System.Linq;
 
 namespace BookCave.Repositories
 {
     public class AuthorRepo
     {
+        private DataContext _db;
+        public AuthorRepo()
+        {
+            _db = new DataContext();
+        } 
         public List<AuthorListViewModel> GetAllAuthors()
         {
-            var authors = new List<AuthorListViewModel>
-            {
-                new AuthorListViewModel { Name = "JK Rowling", Rating = 3.4 }
-            };
+           var authors = (from a in _db.Authors
+                         select new AuthorListViewModel
+                         {
+                             Id = a.Id,
+                             Name = a.Name
+                         }).ToList();
             return authors;
         }
     }
