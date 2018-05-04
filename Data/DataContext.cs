69b0c814822f22f5ebe04af776eb957a5f341b2b
nannalop <natalia17@ru.is>
2018-05-04 12:54:26 +0000

    data fixes

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,6 +5,12 @@ namespace BookCave.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DbSet<Author> Authors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                "Server=tcp:verklegt2.database.windows.net,1433;Initial Catalog=VLN2_2018_H05;Persist Security Info=False;User ID=VLN2_2018_H05_usr;Password=n!cePurple55;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        }
     }
 }
