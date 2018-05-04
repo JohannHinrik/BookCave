58e62444dad2ecf542b0f88dc843a4cb2d6aeeeb
nannalop <natalia17@ru.is>
2018-05-04 11:20:32 +0000

    Author.cs class ready and some DataContext.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
-//nnannanana passewojrwo
 namespace BookCave.Controllers
 {
     public class HomeController : Controller
---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using BookCave.Data.EntityModels;
+
+namespace BookCave.Data
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
