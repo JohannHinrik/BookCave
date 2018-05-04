dede430d94bd54798e4801aa5b6321db2ba3ef81
nannalop <natalia17@ru.is>
2018-05-04 13:48:01 +0000

    database ready?

---
+++ Models/ViewModels/AuthorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookCave.Models.ViewModels
+{
+    public class AuthorListViewModel
+    {
+        public string Name { get; set; }
+        public double Rating { get; set; }
+    }
+}
--- Repositories/AuthorRepo.cs
+++ Repositories/AuthorRepo.cs
@@ -1,16 +1,17 @@
-using System.Collections.Generic
-using BookCave.Models.ViewModels.AuthorViewModel
+using System.Collections.Generic;
+using BookCave.Models.ViewModels;
 
 namespace BookCave.Repositories
 {
     public class AuthorRepo
     {
-        List<AuthorListViewModel> GetAllAuthors()
+        public List<AuthorListViewModel> GetAllAuthors()
         {
-            var authors = new List<AuthorViewModel>
+            var authors = new List<AuthorListViewModel>
             {
-                new AuthorListViewModel
-            }
+                new AuthorListViewModel { Name = "JK Rowling", Rating = 3.4 }
+            };
+            return authors;
         }
     }
 }
