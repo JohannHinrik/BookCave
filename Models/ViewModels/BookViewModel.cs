da9c2390cb3fac65927634128ce8089e2e8de2bc
Guðrún Sara <gudrun.sara@hotmail.com>
2018-05-04 14:55:49 +0000

    BookViewModel order made

--- Models/ViewModels/BookViewModel.cs
+++ Models/ViewModels/BookViewModel.cs
@@ -2,16 +2,17 @@ namespace BookCave.Model.ViewModels
 {
     public class Book
     {
-        public int Author { get; set; }
-
-        public int Review { get; set; }
-
-        public string Genre { get; set; }
 
         public string Title { get; set; }
 
-        public double Rating { get; set; }
+        public string Author { get; set; }
+
+        public string Review { get; set; }
+
+        public string Genre { get; set; }
 
         public string About { get; set; }
+
+        public double Rating { get; set; }
     }
 }
