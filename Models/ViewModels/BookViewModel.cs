4e9b1a81f6772557d92e00ab0f1422ff089cc7f8
Guðrún Sara <gudrun.sara@hotmail.com>
2018-05-04 14:27:55 +0000

    Models-ViewModels-BookViewModel added! aaaaand some HTML work in Views-Book-Index.cshtml

---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,17 @@
+namespace BookCave.Model.ViewModels
+{
+    public class Book
+    {
+        public int Author { get; set; }
+
+        public int Review { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Title { get; set; }
+
+        public double Rating { get; set; }
+
+        public string About { get; set; }
+    }
+}
