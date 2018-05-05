b565e30717f2c2264c5637798b3ab794b47595a0
nannalop <natalia17@ru.is>
2018-05-05 15:47:16 +0000

    fixes to BookViewModel

--- Models/ViewModels/BookViewModel.cs
+++ Models/ViewModels/BookViewModel.cs
@@ -2,17 +2,12 @@ namespace BookCave.Model.ViewModels
 {
     public class BookListViewModel
     {
-
+        public int Isbn { get; set; }
+        public int AuthorId { get; set; }
+        public int ReviewId { get; set; }
         public string Title { get; set; }
-
-        public string Author { get; set; }
-
-        public string Review { get; set; }
-
         public string Genre { get; set; }
-
         public string About { get; set; }
-
         public double Rating { get; set; }
     }
 }
