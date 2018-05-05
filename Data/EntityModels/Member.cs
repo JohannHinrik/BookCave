cf6d1a4f1b69558a2ba42f576f0c6cdbb306204f
Guðrún Sara <gudrun.sara@hotmail.com>
2018-05-05 15:36:42 +0000

    The Entity model: Member.cs added

---
+++ Data/EntityModels/Member.cs
@@ -0,0 +1,15 @@
+namespace BookCave.Data.EntityModels
+{
+    public class Member
+    {
+        public string UserNAme { get; set; }
+        public string Password { get; set; }
+        public string Image { get; set; }
+        public string FavoriteBook { get; set; }
+        public string MemberId { get; set; }
+
+        // public WishList Wislist  { get; set; }
+        // public  Cart Cart { get; set; }
+    }
+  
+}
