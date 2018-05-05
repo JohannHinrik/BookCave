53f1803ee8304a875b1affbb5b5a2be772ffa162
Guðrún Sara <gudrun.sara@hotmail.com>
2018-05-05 11:26:17 +0000

    Bookstuff commented out

--- Repositories/BookRepo.cs
+++ Repositories/BookRepo.cs
@@ -1,18 +1,30 @@
 using System.Collections.Generic;
-using BookCave.Models.ViewModels;
+using BookCave.Model.ViewModels;
+//using BookCave.Models.ViewModels;
 
 namespace BookCave.Repositories
 {
     public class BookRepo
     {
-       /* public List<BookViewModel> GetAllBooks()
+       public List<BookListViewModel> GetAllBooks()
         {
-           var Books = new List<BookViewModel>
+          var Books = new List<BookListViewModel>
             {
-                new BookViewModel { Title = "Prisoner of Azcaban", Author = "J.K. Rowling", Review = "VEry nice", Genre = "Romance", Rating = 10,5, About = "harry potter is the boy who lived, read all about it" },
-                new BookViewModel { Title = "Notting hill", Author = "Gudrun Sara", Review = "More nice", Genre = "Romance", Rating = 9,5, About = "bets book ever" }
+                new BookListViewModel { Title = "Prisoner of Azcaban", 
+                                    Author = "J.K. Rowling", 
+                                    Review = "VEry nice", 
+                                    Genre = "Romance", 
+                                    About = "harry potter is the boy who lived, read all about it", 
+                                    Rating = 10.5 }
+                                    
+               /* new BookViewModel { Title = "Notting hill",
+                                    Author = "Gudrun Sara", 
+                                    Review = "More nice", 
+                                    Genre = "Romance", 
+                                    Rating = 9.5, 
+                                    About = "bets book ever" }*/
             };
             return Books; 
-        }*/
+        }
     }
 }
