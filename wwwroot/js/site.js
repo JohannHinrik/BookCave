dfef3bf6cc539c64c9985f218a470cc3481cbbfc
Hugrún Hannesdóttir <hugrun17@ru.is>
2018-05-08 14:43:42 +0000

    animation on button click and icon added next to cart

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,40 @@
 $('#icon').click(function() { 
     document.location = 'http://localhost:5000';
 } );
+
+/* $('.cartButton').delegate(".bookTitle", 'click', function(){
+    swal({
+        title: "Book has been added to your cart!",
+        text: "Do you wish to go to cart or continue shopping?",
+        icon: "success",
+        button: ["Continue shopping", "Go to cart"],
+  }).then((value) =>{
+      if(value == null)
+      {
+          swal("Continue shopping");
+      }
+      else
+      {
+          swal("Go to cart");
+      }
+  });
+}); */
+
+
+$('.cartButton').click(function() { 
+    swal({
+        title: "Success!",
+        text: "Book has been added to your cart!",
+        icon: "success",
+        button: "Continue Browsing",
+      });
+});
+
+$('.wishListButton').click(function() { 
+    swal({
+        title: "Success!",
+        text: "Book has been added to your wishlist!",
+        icon: "success",
+        button: "Continue Browsing",
+      });
+});
