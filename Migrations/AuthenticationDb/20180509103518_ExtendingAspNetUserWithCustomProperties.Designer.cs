7afad9bdb7190794b3c0ddfadbd2d13766a320fd
Guðrún Sara <gudrun.sara@hotmail.com>
2018-05-09 10:43:58 +0000

    Adding custom properties to user

--- Migrations/AuthenticationDb/20180509103518_ExtendingAspNetUserWithCustomProperties.Designer.cs
+++ Migrations/AuthenticationDb/20180509103518_ExtendingAspNetUserWithCustomProperties.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BookCave.Migrations.AuthenticationDb
 {
     [DbContext(typeof(AuthenticationDbContext))]
-    partial class AuthenticationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180509103518_ExtendingAspNetUserWithCustomProperties")]
+    partial class ExtendingAspNetUserWithCustomProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,16 @@ namespace BookCave.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        
+        //Adding properties to a user: 
+
+        public string FirstName {get; set; }
+        public string LastName {get; set; }
+        public string FavoriteBook {get; set; }
+
+        // Without Arnar: 
+        public string Username {get; set; }
+        public string City {get; set; }
+        public string Country {get; set; }
+        public string Address {get; set; }
     }
 }
