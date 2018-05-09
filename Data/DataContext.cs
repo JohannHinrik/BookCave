8e3e157a86ddd19005a43b305a4fbe574d718632
nannalop <natalia17@ru.is>
2018-05-09 18:43:37 +0000

    DataContext updated with cart and orders

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,6 +8,9 @@ namespace BookCave.Data
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Review> Reviews { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
