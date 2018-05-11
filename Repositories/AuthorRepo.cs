6204c4f99eeb493b47265206843a56e274575664
unknown <johannhinrik@gmail.com>
2018-05-11 21:40:34 +0000

    fixed minor indenting

--- Repositories/AuthorRepo.cs
+++ Repositories/AuthorRepo.cs
@@ -32,26 +32,26 @@ namespace BookCave.Repositories
         public List<AuthorListViewModel> GetTopRatedAuthors()
         {
             // Gets a list of all the user ratings the author has resieved for all his books
-            var topRatedBooks = ( from b in _db.Books
-                                    join r in _db.Reviews on b.Id equals r.BookId
-                                    select new AuthorListViewModel
-                                    {
-                                        Id = b.AuthorId,
-                                        Rating = r.Rating
-                                    });
-                                    
-            
+            var topRatedBooks = (from b in _db.Books
+                                 join r in _db.Reviews on b.Id equals r.BookId
+                                 select new AuthorListViewModel
+                                 {
+                                     Id = b.AuthorId,
+                                     Rating = r.Rating
+                                 });
+
+
             // Top rated authors are calculated from the table above,
             // by getting the average of the authors user ratings
-            var topRatedAuthors = ( from a in _db.Authors
-                                    join b in topRatedBooks on a.Id equals b.Id
-                                    orderby b.Rating descending
-                                    group b by new { b.Id, a.Name } into BooksRated
-                                    select new AuthorListViewModel
-                                    {
-                                        Name = BooksRated.Key.Name,
-                                        Rating = BooksRated.Average(a => a.Rating)
-                                    });
+            var topRatedAuthors = (from a in _db.Authors
+                                   join b in topRatedBooks on a.Id equals b.Id
+                                   orderby b.Rating descending
+                                   group b by new { b.Id, a.Name } into BooksRated
+                                   select new AuthorListViewModel
+                                   {
+                                       Name = BooksRated.Key.Name,
+                                       Rating = BooksRated.Average(a => a.Rating)
+                                   });
 
             // Making sure the list is ordered correctly
             var topTen = topRatedAuthors.OrderByDescending(average => average.Rating);
