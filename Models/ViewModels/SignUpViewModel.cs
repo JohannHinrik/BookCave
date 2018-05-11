b92445db012ae3a3b7f69d060ce579f247c7ee02
nannalop <natalia17@ru.is>
2018-05-11 11:12:18 +0000

    login requuirement fields

--- Models/ViewModels/SignUpViewModel.cs
+++ Models/ViewModels/SignUpViewModel.cs
@@ -7,18 +7,25 @@ namespace BookCave.Models.ViewModels
         [Required(ErrorMessage = "Email field is required")]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         public string UserName { get; set; }
+
         [Required(ErrorMessage = "First Name field is required")]
         public string FirstName { get; set; }
+
         [Required(ErrorMessage = "Last Name field is required")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "Address field is required")]
         public string Address { get; set; }
+
         [Required(ErrorMessage = "City field is required")]
         public string City { get; set; }
+
         [Required(ErrorMessage = "Country field is required")]
         public string Country { get; set; }
+        
         [Required(ErrorMessage = "Password field is required")]
         public string Password { get; set; }
     }
